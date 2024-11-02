c5727f5b856da61b3229d2a038dd24a1d9e10ae7
Artem Bazarov <bazarow-artem@mail.ru>
2024-11-02 16:38:12 +0300

    4-navigator

--- 6-Drivers/main.js
+++
@@ -1,9 +0,0 @@
-
-
-const hasLicence = true;
-const age = 19;
-const isDrunk = false;
-
-const possibilityToDrive = (age >=18 && hasLicence && !isDrunk)
-
-console.log(`Возможность вести машину: ${possibilityToDrive ? 'Может' : 'Не может'}`)
