bed420c6d93b1ddb5b3327f10270074a03a7d848
Artem Bazarov <bazarow-artem@mail.ru>
2024-12-06 21:59:14 +0300

    Fix-troubles

--- 16- another task/main.js
+++ 16- another task/main.js
@@ -1,32 +1,24 @@
 const toDoList = {
   tasks: [],
   id: 1,
-  description: "",
-  order: 0,
-  addTask: function (titleName, priority, descr) {
+  addTask: function (titleName, priority) {
     const newTask = {
-      title: titleName,
       id: this.id++,
+      title: titleName,
       priority,
-      order: this.order++,
-      description: descr,
     };
     this.tasks.push(newTask);
     return newTask;
   },
   deleteTask: function (id) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
-    this.tasks.forEach((task, i) => {
-      task.id = i + 1;
-      task.order = i + 1;
-    });
   },
-  updateTask: function (id, title, priority, descr) {
+
+  updateTask: function (id, title, priority) {
     const task = this.tasks.find((task) => task.id === id);
     if (task) {
       task.title = title;
       task.priority = priority;
-      task.description = descr;
       return task;
     }
     return null;
@@ -36,13 +28,46 @@ const toDoList = {
     this.tasks = this.tasks.sort((a, b) => a.id < b.id);
   },
 };
-toDoList.addTask("Помыть посуду", 1, "Это далось мне очень тяжело");
-toDoList.addTask("Постирать", 2, "Из последних сил");
-toDoList.addTask("Сходить в магазин", 6, "С ненавистью и превозмогая усталось");
-toDoList.addTask("Погладить", 3, "Все как в тумане");
-toDoList.addTask("Прочесть книгу", 5, "Хватило на целых 5 мин");
-toDoList.updateTask(1, "Пропылесосить", 10, "Было лучше");
-toDoList.sortTasks();
-toDoList.deleteTask(2);
+// toDoList.addTask("Помыть посуду", 1);
+// toDoList.addTask("Постирать", 2);
+// toDoList.addTask("Сходить в магазин", 6);
+// toDoList.addTask("Погладить", 3);
+// toDoList.addTask("Прочесть книгу", 5);
+// toDoList.updateTask(1, "Пропылесосить", 10);
+// toDoList.sortTasks();
+// toDoList.deleteTask(2);
 
-console.log(toDoList.tasks);
+// console.log(toDoList.tasks);
+
+const newToDoList = {
+  tasks: [],
+  id: 1,
+  description: "",
+  order: 0,
+};
+
+function addDescriptionAndOrder(titleName, priority, description, order) {
+  const newTask = toDoList.addTask.call(newToDoList, titleName, priority);
+  newTask.description = description;
+  newTask.order = order;
+  return newTask;
+}
+
+function updateDescriptionAndTitle(id, titleName, description, priority) {
+  const newTask = toDoList.updateTask.call(newToDoList, id, titleName, priority);
+  newTask.description = description;
+  return newTask;
+}
+
+newToDoList.sortTasks = toDoList.sortTasks;
+newToDoList.deleteTask = toDoList.deleteTask;
+
+addDescriptionAndOrder("Постирать", 2, "Из последних сил", 2);
+addDescriptionAndOrder("Помыть посуду", 1, "Перенести на завтра", 1);
+addDescriptionAndOrder("Сходить в магазин", 10, "Обязательно сегодня", 3);
+addDescriptionAndOrder("Погладить", 4, "Завтра");
+newToDoList.sortTasks();
+updateDescriptionAndTitle(1, "Пропылесосить", "Было лучше", 4);
+newToDoList.deleteTask(2);
+
+console.log(newToDoList.tasks);
